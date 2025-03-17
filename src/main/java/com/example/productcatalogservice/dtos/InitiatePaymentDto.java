e33f72fa5b56ff9e2d4e69e864fd91f296f9ee8f
gvs9 <sakharkargitesh9@gmail.com>
2025-03-17 19:30:48 +0530

    implemented payment service with Razorpay  for pcs

---
+++ src/main/java/com/example/productcatalogservice/dtos/InitiatePaymentDto.java
@@ -0,0 +1,19 @@
+package com.example.productcatalogservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InitiatePaymentDto {
+    private Long amount;
+
+    private String orderId;
+
+    private String phoneNumber;
+
+    private String name;
+
+    private String email;
+
+}
---
+++ src/main/java/com/example/productcatalogservice/paymentgateway/PaymentGatewayChooserStrategy.java
@@ -0,0 +1,15 @@
+package com.example.productcatalogservice.paymentgateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentGatewayChooserStrategy {
+
+@Autowired
+    private  RazorpayPaymentGateway razorpayPaymentGateway;
+
+public  PaymentGateway getOptimalPaymentGateway() {
+    return razorpayPaymentGateway;
+}
+}
