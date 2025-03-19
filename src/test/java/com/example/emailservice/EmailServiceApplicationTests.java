66a73a2c0a59fbc44078965d60696ae04c591fbc
gvs9 <sakharkargitesh9@gmail.com>
2025-03-19 22:00:30 +0530

    implemented email service

---
+++ src/test/java/com/example/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+
+}
