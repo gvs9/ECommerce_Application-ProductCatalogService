6a1e89fe911e12fdbc73ee30db9fa86ee857f186
gvs9 <sakharkargitesh9@gmail.com>
2025-03-23 00:41:25 +0530

    implemented product catalog service

--- src/main/java/com/example/productcatalogservice/paymentgateway/PaymentGateway.java
+++
@@ -1,6 +0,0 @@
-package com.example.productcatalogservice.paymentgateway;
-
-public interface PaymentGateway {
-
-    String getPaymentLink(Long amount,String orderId,String phoneNumber,String name,String email);
-}
